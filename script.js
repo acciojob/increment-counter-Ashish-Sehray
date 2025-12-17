3566dc01ff7186a7a21035575f36c09d96a1a605
Ashish-Sehray <sehrayashish54@gmail.com>
2025-12-17 22:19:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let p =document.querySelector("#counter");
+let button = document.querySelector("#incrementBtn");
+let counter=0;
+button.addEventListener("click",()=>{
+	counter+=1;
+	alert(`${counter}`);
+	p.textContent=`${counter}`;
+})
